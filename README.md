03b8b37d92b87e7062141e45a3cf6776cd678230
itxx00 <itxx00@gmail.com>
2015-06-16 14:40:30 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # zabbix_app_named
 zabbix template to monitor bind9 named service
+
+# How to use (tested on CentOS7)
+the monitor script need to use zabbix-sender
+
+bash```
+yum install zabbix-sender
+```
+
+configure named.conf in _options_ section:
+
+```
+statistics-file "/var/named/data/named_stats.txt";
+```
+
+import template xml in zabbix dashboard and,
+create a crontab on named server
+
+```
+cat >/etc/cron.d/zabbix-named <<EOF
+* * * * * root /opt/zabbix/zabbix-named.sh >/dev/null 2>&1 &
+EOF
+```
---
+++ zabbix-named.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# send named metrics to zabbix
+
+ZAB_CONF=/etc/zabbix/zabbix_agentd.conf
+ZAB_BIN=/usr/bin/zabbix_sender
+RNDC_BIN=/usr/sbin/rndc
+STATS='/var/named/data/named_stats.txt'
+TMP_FILE=$(mktemp)
+
+rm -f $STATS
+if ! $RNDC_BIN stats; then
+    exit 1
+fi
+if [ -s ${STATS} ]; then
+    named_total_query=$(awk '/ QUERY/ {print $1}' $STATS)
+    named_ipv4_request=$(awk '/ IPv4 requests received/ {print $1}' $STATS)
+    named_tcp_request=$(awk '/ TCP requests received/ {print $1}' $STATS)
+    named_total_response=$(awk '/[0-9] responses sent/ {print $1}' $STATS)
+    named_success_query=$(awk '/queries resulted in successful/ {print $1}' $STATS)
+    named_dropped_query=$(awk '/ queries dropped/ {print $1}' $STATS)
+    echo "- named_total_query $named_total_query" >$TMP_FILE
+    echo "- named_ipv4_request $named_ipv4_request" >>$TMP_FILE
+    echo "- named_tcp_request $named_tcp_request" >>$TMP_FILE
+    echo "- named_total_response $named_total_response" >>$TMP_FILE
+    echo "- named_success_query $named_success_query" >>$TMP_FILE
+    echo "- named_dropped_query $named_dropped_query" >>$TMP_FILE
+    if tty >/dev/null; then
+        cat ${TMP_FILE}
+        $ZAB_BIN -vv -c ${ZAB_CONF} -i ${TMP_FILE}
+    else
+        $ZAB_BIN -c ${ZAB_CONF} -i ${TMP_FILE} >/dev/null
+    fi
+    rm -f ${TMP_FILE}
+elif tty >/dev/null ; then
+    echo "${STATS} does not exist!" >&2
+    rm -f ${TMP_FILE}
+    exit 1
+fi
