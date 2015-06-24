0bf7aba96bc21d5a82e337b5e0390837626e3838
itxx00 <itxx00@gmail.com>
2015-06-24 16:01:14 +0800

    remove named_ipv4_request item

--- zabbix-named.sh
+++ zabbix-named.sh
@@ -13,13 +13,11 @@ if ! $RNDC_BIN stats; then
 fi
 if [ -s ${STATS} ]; then
     named_total_query=$(awk '/ QUERY/ {print $1}' $STATS)
-    named_ipv4_request=$(awk '/ IPv4 requests received/ {print $1}' $STATS)
     named_tcp_request=$(awk '/ TCP requests received/ {print $1}' $STATS)
     named_total_response=$(awk '/[0-9] responses sent/ {print $1}' $STATS)
     named_success_query=$(awk '/queries resulted in successful/ {print $1}' $STATS)
     named_dropped_query=$(awk '/ queries dropped/ {print $1}' $STATS)
     echo "- named_total_query $named_total_query" >$TMP_FILE
-    echo "- named_ipv4_request $named_ipv4_request" >>$TMP_FILE
     echo "- named_tcp_request $named_tcp_request" >>$TMP_FILE
     echo "- named_total_response $named_total_response" >>$TMP_FILE
     echo "- named_success_query $named_success_query" >>$TMP_FILE
